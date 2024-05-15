98b5097731ad1e882282b6ccda4f9fbe77731b4b
Yulia Shyshka <shyshka20@gmail.com>
2024-05-15 23:23:50 +0300

    add changes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import eventsRoute from "./routes/eventsRoute.js";
 import cors from "cors";
 import agenda from "./agenda/agenda.js";
 import dotenv from "dotenv";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
