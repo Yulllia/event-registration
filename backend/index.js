8903eaff7b1d4f98063b4a62b212ef2c061e5126
Yulia Shyshka <shyshka20@gmail.com>
2024-05-15 23:31:55 +0300

    add changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ import eventsRoute from "./routes/eventsRoute.js";
 import cors from "cors";
 import agenda from "./agenda/agenda.js";
 import dotenv from "dotenv";
-import path from "path";
 
 dotenv.config();
 
@@ -19,14 +18,6 @@ app.use(cors());
 
 app.use("/events", eventsRoute);
 app.use("/register", registerRoute);
-
-if ((process.env.NODE_ENV = "production")) {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 mongoose
   .connect(mongoDBURL)
